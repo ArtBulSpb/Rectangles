ec75d0649f0ac83affcccbe05518511e2fd3deea
ArtBulSpb <arturtomsk2005@gmail.com>
2022-09-23 22:07:40 +0300

    Colored text

--- asa.py
+++ asa.py
@@ -15,7 +15,6 @@ class Rectangle(object):
         else:
             return "Нет"
 
-
     def is_intersected(self, other):
         if any([all([other.x1 <= self.x1 <= other.x2, other.y1 <= self.y1 <= other.y2]),
                 all([other.x1 <= self.x2 <= other.x2, other.y1 <= self.y1 <= other.y2]),
@@ -24,17 +23,18 @@ class Rectangle(object):
             return "Нет"
         return "Да"
 
-print("Введите координаты для")
-print("Прямоугольника 1:")
-rect1 = Rectangle(int(input("x левого нижнего угла = ")), int(input("y левого нижнего угла = ")), int(input("ширина = ")), int(input("высота = ")))
-print()
-print("Прямоугольника 2:")
-rect2 = Rectangle(int(input("x левого нижнего угла = ")), int(input("y левого нижнего угла = ")), int(input("ширина = ")), int(input("высота = ")))
 
-print()
-print('а) Принадлежат ли все точки первого прямоугольника второму:')
+print("\033[30m\033[46m{}".format("Введите координаты для"))
+print("Прямоугольника 1:")
+rect1 = Rectangle(int(input("\033[30m\033[46m{}".format("x левого нижнего угла = "))),
+                  int(input("y левого нижнего угла = ")),
+                  int(input("ширина = ")), int(input("высота = ")))
+print("\n\033[40m\033[3m\033[34m{}".format("Прямоугольника 2:"))
+rect2 = Rectangle(int(input("x левого нижнего угла = ")), int(input("y левого нижнего угла = ")),
+                  int(input("ширина = ")), int(input("высота = ")))
+print("\033[41m\033[1m\033[7m\033[34m{}".format("а) Принадлежат ли все точки первого прямоугольника второму:"))
 print(rect2.is_involved(rect1))
-print('б) Принадлежат ли все точки одного из прямоугольников другому:')
+print("\033[47m\033[4m\033[35m{}".format("б) Принадлежат ли все точки одного из прямоугольников другому:"))
 print(rect2.is_involved(rect1) or rect1.is_involved(rect2))
-print('в) Пересекаются ли эти прямоугольники:')
+print("\033[43m\033[5m\033[4m\033[35m{}".format("в) Пересекаются ли эти прямоугольники:"))
 print(rect2.is_intersected(rect1) or rect1.is_intersected(rect2))
